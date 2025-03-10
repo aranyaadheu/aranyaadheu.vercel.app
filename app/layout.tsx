de35873818f6ef333059c90a579ffcbe31031d6b
Aniruddha Aranya A Dheu <125354418+aranyaadheu@users.noreply.github.com>
2025-03-11 03:08:18 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+       <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon/apple-icon-180x180.png" />
+      </head>
       <body className="min-h-screen bg-white dark:bg-[#212737] text-zinc-900 dark:text-zinc-100 font-sans antialiased">
         <div className="max-w-2xl mx-auto px-6 py-12">
           {children}
