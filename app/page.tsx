4bf59a3b332a370946880126c7e08cc986dec0bf
Aniruddha Aranya A Dheu <aranyaaniruddha@gmail.com>
2025-03-11 23:26:26 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
             <span className="mr-2">♟️</span> Chess Analysis and Strategies
           </motion.li>
           <motion.li className="flex items-center" variants={fadeInUp}>
-            <span className="mr-2">⚙️</span> developing and optimizing prompts to efficiently apply and build with LLMs for a wide variety of applications and use cases. 
+            <span className="mr-2">⚙️</span> Developing and optimizing prompts to efficiently apply and build with LLMs for a wide variety of applications and use cases. 
           </motion.li>
           <motion.li className="flex items-center" variants={fadeInUp}>
             <span className="mr-2">💻</span> Competitive Programming Tips
