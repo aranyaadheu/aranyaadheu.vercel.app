d9097e06769bf5c4307cf7a706238287e0fb2931
Aniruddha Aranya A Dheu <aranyaaniruddha@gmail.com>
2025-03-13 15:47:27 +0600

    Updates

---
+++ _next/static/vCMHXUEJ1htAsypTj6vbh/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fapi\u002Fposts\u002F[slug]","\u002Fapi\u002Fposts\u002F[slug]\u002Fcontent","\u002Fapi\u002Ftags\u002F[tag]","\u002Fposts\u002F[slug]","\u002Ftags\u002F[tag]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
